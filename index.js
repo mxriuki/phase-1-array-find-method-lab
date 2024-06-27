9c82284f983c8cc3537ca0bd5516c693c6124c58
mxriuki <mwendav37@gmail.com>
2024-06-27 09:29:57 +0300

    solution

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record){
+    let result = record.find((item)=>item.result == 'W')
+    if(result !== undefined)
+        return result.year
+    else 
+        return undefined
+}
